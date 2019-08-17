50d5ea3f34f3c6f10391c0d2422fd31e29f2bbcf
phidoit <phidoit@gmail.com>
2019-08-18 01:04:46 +0800

    --init Project--

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print('Hello World')
+import tushare as ts
+
+# print('Hello World')
+# print(ts.__version__)
+
+etf50 = ts.get_hist_data('510050',start='2016-01-01',end='2019-08-09')
+etf50.to_excel('D:/StockPlace/etf50.xlsx')
+#print(etf50)
